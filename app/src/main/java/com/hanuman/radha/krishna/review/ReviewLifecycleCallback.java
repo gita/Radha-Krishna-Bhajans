0b8bb245848ef798bffe83dab27e56282e71cae1
hussain <vohrah220@gmail.com>
2021-08-28 18:07:03 +0530

    Cast Exception Crash Fix.

--- app/src/main/java/com/hanuman/radha/krishna/review/ReviewLifecycleCallback.java
+++ app/src/main/java/com/hanuman/radha/krishna/review/ReviewLifecycleCallback.java
@@ -14,16 +14,20 @@ import com.suddenh4x.ratingdialog.preferences.RatingThreshold;
 public class ReviewLifecycleCallback implements Application.ActivityLifecycleCallbacks {
     @Override
     public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle bundle) {
-        if(bundle == null) {
-            AppCompatActivity activity1 = (AppCompatActivity) activity;
-            new AppRating.Builder(activity1)
-                    .setMinimumLaunchTimes(5)
-                    .setMinimumDays(2)
-                    .setMinimumLaunchTimesToShowAgain(5)
-                    .setMinimumDaysToShowAgain(10)
-                    .setRatingThreshold(RatingThreshold.FOUR)
-                    .useGoogleInAppReview()
-                    .showIfMeetsConditions();
+        if (bundle == null) {
+            try {
+                AppCompatActivity activity1 = (AppCompatActivity) activity;
+                new AppRating.Builder(activity1)
+                        .setMinimumLaunchTimes(5)
+                        .setMinimumDays(2)
+                        .setMinimumLaunchTimesToShowAgain(5)
+                        .setMinimumDaysToShowAgain(10)
+                        .setRatingThreshold(RatingThreshold.FOUR)
+                        .useGoogleInAppReview()
+                        .showIfMeetsConditions();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
